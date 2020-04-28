7dc0f9c5955ce39e50b8cf46f9b1fdcda14cba20
Name <Email>
2020-04-28 16:49:10 +0300

    up7

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Title({ name, title }) {
   return (
-    <div className="row" >
-      <div className="col-10 mx-auto my-4 text-center text-title">
+    <div>
+      <div className="text-title">
       <img className="puzzle-icon" src={logo}></img>
         <h1 className="title-size">
           {name} <strong className="text-blue">{title}</strong>
