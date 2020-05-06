0f91a3bf2b9af9da9be63f1587aff8862ce72a85
Name <Email>
2020-05-06 23:44:55 +0300

    h

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -35,7 +35,7 @@ export default class MyApp extends React.Component {
       // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
     };
 
-    let env = "production"; // you can set here to 'production' for production
+    let env = "sandbox"; // you can set here to 'production' for production
     let currency = "ILS"; // or you can set this value from your props or state
     //let total = 1;
     // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
